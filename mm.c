01a859b53c1785eb60d205cf23658bc0785f6866
Vincent <36722479+Vinnyhd@users.noreply.github.com>
2018-12-01 09:31:28 -0700

    Update mm.c

--- mm.c
+++ mm.c
@@ -1,3 +1,4 @@
+
 /* 
  * mm-implicit.c -  Simple allocator based on implicit free lists, 
  *                  first fit placement, and boundary tag coalescing. 
@@ -153,10 +154,19 @@ static void checkblock(void *bp);
 
 int mm_init(void) 
 {
-  //
-  // You need to provide this
-  //
-  return 0;
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void*) -1)
+        return -1;
+    
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3*WSIZE), PACK(0,1));
+    
+    heap_listp += (2*WSIZE);
+    
+    if(extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
+    return 0;
 }
 
 
@@ -165,10 +175,17 @@ int mm_init(void)
 //
 static void *extend_heap(uint32_t words) 
 {
-  //
-  // You need to provide this
-  //
-  return NULL;
+    char *bp;
+    size_t size;
+    
+    size = (words%2) ? (words+1) * WSIZE : words * WSIZE;
+    if((long)(bp = mem_sbrk(size)) == -1)
+        return NULL
+    
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FDRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1));
+    return coalesce(bp);
 }
 
 
@@ -189,9 +206,11 @@ static void *find_fit(uint32_t asize)
 
 void mm_free(void *bp)
 {
-  //
-  // You need to provide this
-  //
+    size_t size = GETSIZE(HDRP(bp));
+    
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FDRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
 
 //
@@ -199,7 +218,33 @@ void mm_free(void *bp)
 //
 static void *coalesce(void *bp) 
 {
-  return bp;
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+    
+    if(prev_alloc && next_alloc)\
+        return bp;
+    else if(prev_alloc && !next_alloc)
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    else if(!prev_alloc && next_alloc)
+    {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    else
+    {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
 }
 
 // mm_malloc: The mm_malloc routine returns a pointer to an allocated 
@@ -213,10 +258,30 @@ static void *coalesce(void *bp)
 
 void *mm_malloc(uint32_t size) 
 {
-  //
-  // You need to provide this
-  //
-  return NULL;
+    size_t asize;
+    size_t extendsize;
+    char* bp;
+    
+    if(size == 0)
+        return NULL
+    
+    if(size <= DSIZE)
+        asize = 2*DSIZE;
+    else
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+    
+    if((bp = find_fit(asize)) != NULL)
+    {
+        place(bp, asize);
+        return bp;
+    }
+    
+    extendsize = MAX(asize, CHUNKSIZE);
+    if((bp = extend_heap(extendsize/WSIZE)) == NULL)
+        return NULL
+    
+    place(bp, asize);
+    return bp;
 } 
 
 //
